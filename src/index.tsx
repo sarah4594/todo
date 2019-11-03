c2dafde275226c0faf4c91438de672e52eac5f0b
sarah4594 <sarah4594@gmail.com>
2019-11-03 16:31:01 -0600

    ✨ Link overmind to react with Provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { overmind } from './overmind'
+import { Provider } from 'overmind-react'
 import './index.css'
 import App from './components/App'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <Provider value={overmind}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/overmind/index.ts
+++ src/overmind/index.ts
@@ -1,5 +1,5 @@
-import { IConfig } from 'overmind'
-
+import { IConfig, createOvermind } from 'overmind'
+import { createHook } from 'overmind-react'
 import { state } from './state'
 import * as actions from './actions'
 
@@ -11,3 +11,7 @@ export const config = {
 declare module 'overmind' {
   interface Config extends IConfig<typeof config> {}
 }
+
+export const overmind = createOvermind(config)
+
+export const useOvermind = createHook<typeof config>()
