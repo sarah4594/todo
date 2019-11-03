17c02361270d7a4bd25a306c33e6669ace20fd34
sarah4594 <sarah4594@gmail.com>
2019-11-03 11:51:30 -0600

    ✅ Add test for adding actions

---
+++ src/overmind/actions.test.ts
@@ -0,0 +1,22 @@
+import { createOvermindMock } from 'overmind'
+import { config } from './'
+
+it('should add a todo', () => {
+  const { state, actions } = createOvermindMock(config)
+  const title = 'new todo'
+  actions.addTodo(title)
+  console.log(state.todos)
+  expect(
+    Object.values(state.todos).find(todo => todo.title === title),
+  ).toBeTruthy()
+})
+
+it('should add 2 todos', () => {
+  const { state, actions } = createOvermindMock(config)
+  const title1 = 'new todo 1'
+  const title2 = 'new todo 2'
+  actions.addTodo(title1)
+  actions.addTodo(title2)
+  console.log(state.todos)
+  expect(Object.values(state.todos).length).toBe(2)
+})
--- src/overmind/actions.ts
+++ src/overmind/actions.ts
@@ -1,3 +1,7 @@
 import { Action } from 'overmind'
+import Utils from '../Utils'
 
-export const addTodo: Action<string> = ({ state }, title) => {}
+export const addTodo: Action<string> = ({ state }, title) => {
+  const id = Utils.uuid()
+  state.todos[id] = { id, title, completed: false }
+}
