36d0c9eb9ca87731a13fef3ae30ff78c9855188f
sarah4594 <sarah4594@gmail.com>
2019-11-03 16:44:54 -0600

    ✨ Render todos from overmind

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react'
 import logo from '../logo.svg'
 import './App.css'
+import { useOvermind } from '../overmind'
+import { addTodo } from '../overmind/actions'
 
 const App: React.FC = () => {
+  const { state, actions } = useOvermind()
+  actions.addTodo('first todo')
+  actions.addTodo('second todo')
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {Object.values(state.todos).map(todo => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
     </div>
   )
 }
