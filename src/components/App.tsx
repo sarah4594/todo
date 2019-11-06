8ca68be430408955f2043881f4a6c2ba5ed6b6b9
sarah4594 <sarah4594@gmail.com>
2019-11-05 21:24:18 -0600

    🚧✨ Start to add checkboxes for toggleTodo

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import { useOvermind } from '../overmind'
+import { toggleTodo } from '../overmind/actions'
 
 const App: React.FC = () => {
   const { state } = useOvermind()
+  const handleClick = () => {
+    actions.toggleTodo(todo.id)
+  }
 
   return (
     <div>
       <ul>
         {Object.values(state.todos).map(todo => (
           <li key={todo.id}>
-            {todo.title} {todo.completed ? 'yes' : 'no'}
+            {todo.title}
+            <input type="checkbox" id={todo.id} onClick={handleClick()} />
+            {todo.completed ? 'yes' : 'no'}
           </li>
         ))}
       </ul>
