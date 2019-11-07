509d9fe376c3586a670aa871e69d634c910ec58a
sarah4594 <sarah4594@gmail.com>
2019-11-06 21:37:16 -0600

    ✅ Update App test

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import App from './App'
+import { createOvermindMock } from 'overmind'
+import { Provider } from 'overmind-react'
+import { config } from '../overmind'
 
 it('renders without crashing', () => {
-  const message = 'Learn React'
-  const { getByText } = render(<App />)
-  expect(getByText(message))
+  const overmind = createOvermindMock(config)
+  render(
+    <Provider value={overmind}>
+      <App />
+    </Provider>,
+  )
 })
