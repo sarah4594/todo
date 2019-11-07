8ab4381e88b29f1db5e67b576a2cb1427d278bc3
sarah4594 <sarah4594@gmail.com>
2019-11-06 21:36:57 -0600

    ✅ Update actions test

--- src/overmind/actions.test.ts
+++ src/overmind/actions.test.ts
@@ -2,27 +2,31 @@ import { createOvermindMock } from 'overmind'
 import { config } from './'
 
 it('should add a todo', () => {
-  const { state, actions } = createOvermindMock(config)
+  const { state, actions } = createOvermindMock(config, {
+    storeTodos: jest.fn(),
+  })
   const title = 'new todo'
   actions.addTodo(title)
   console.log(state.todos)
-  expect(
-    Object.values(state.todos).find(todo => todo.title === title),
-  ).toBeTruthy()
+  expect(state.todoList.find(todo => todo.title === title)).toBeTruthy()
 })
 
 it('should add 2 todos', () => {
-  const { state, actions } = createOvermindMock(config)
+  const { state, actions } = createOvermindMock(config, {
+    storeTodos: jest.fn(),
+  })
   const title1 = 'new todo 1'
   const title2 = 'new todo 2'
   actions.addTodo(title1)
   actions.addTodo(title2)
   console.log(state.todos)
-  expect(Object.values(state.todos).length).toBe(2)
+  expect(state.todoList.length).toBe(2)
 })
 
 it('should toggle completed', () => {
-  const { state, actions } = createOvermindMock(config)
+  const { state, actions } = createOvermindMock(config, {
+    storeTodos: jest.fn(),
+  })
   const title = 'new todo'
   const todoId = actions.addTodo(title)
   expect(state.todos[todoId].completed).toBe(false)
