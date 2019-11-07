2e8e117dc44f65551325fd20f272d4e99600b669
sarah4594 <sarah4594@gmail.com>
2019-11-06 20:08:52 -0600

    ✨ Create button to add todos

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,10 +3,17 @@ import { useOvermind } from '../overmind'
 import TodoItem from './TodoItem'
 
 const App: React.FC = () => {
-  const { state } = useOvermind()
+  const { state, actions } = useOvermind()
+  const [count, setCount] = React.useState(1)
+
+  const handleClick = () => {
+    actions.addTodo(`Todo #${count}`)
+    setCount(count + 1)
+  }
 
   return (
     <div>
+      <button onClick={handleClick}>Add Todo</button>
       <ul>
         {Object.values(state.todos).map(todo => (
           <TodoItem key={todo.id} todo={todo} />
