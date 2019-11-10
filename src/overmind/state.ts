c9650dae720d2c5b78801e45f4c60bd3c3f39f02
sarah4594 <sarah4594@gmail.com>
2019-11-10 11:18:10 -0600

    ✨ Add filtered list

--- src/overmind/state.ts
+++ src/overmind/state.ts
@@ -12,9 +12,23 @@ export type State = {
   }
   todoList: Derive<State, Todo[]>
   editingTodoId?: string
+  filter: 'all' | 'active' | 'completed'
+  filteredList: Derive<State, Todo[]>
 }
 
 export const state: State = {
   todos: {},
   todoList: ({ todos }) => Object.values(todos),
+  filter: 'all',
+  filteredList: ({ todos, filter }) =>
+    Object.values(todos).filter(todo => {
+      switch (filter) {
+        case 'all':
+          return true
+        case 'active':
+          return !todo.completed
+        case 'completed':
+          return todo.completed
+      }
+    }),
 }
